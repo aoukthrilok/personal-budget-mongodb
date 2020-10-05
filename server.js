fd97395df4955f1887feefe1bfe8df0667be904f
aoukthrilok <aoukthrilok@outlook.com>
2020-10-04 22:57:01 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,7 @@ const port = 3000;
 
 app.use('/',express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title:'Eat out',
-        budget:40
-    },
-    {
-        title:'Rent',
-        budget:375
-    },
-    {
-        title:'Groceries',
-        budget:105
-    },
-]
-};
+const budget = require('./my_budget.json');
 
 app.get('/hello',(req,res) => {
     res.send('Hello World!');
